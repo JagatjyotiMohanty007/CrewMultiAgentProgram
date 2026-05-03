02611d1a5ae77388cbeee14f1cbc90289f4ded39
Jagatjyoti Mohanty <mohanty.jagatjyoti@gmail.com>
2026-05-03 20:35:34 +0530

    Add files via upload

---
+++ CrewMultiAgent.py
@@ -0,0 +1,103 @@
+# !nvidia-smi
+
+#!pip install crewai
+
+#!pip install 'crewai[tools]'
+# !pip install crewai-tools
+# !pip install crewai-tools
+# !pip install --upgrade crewai crewai-tools
+# !pip install crewai langchain-google-genai
+# pip install litellm
+# pip uninstall crewai litellm -y
+# pip install crewai litellm
+
+
+from re import VERBOSE
+import os
+from crewai import Agent, Task, Crew
+from crewai_tools import SerperDevTool
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+os.environ["LITELLM_PROVIDER"] = "gemini"
+# os.environ['OPENAI_API_KEY'] = 'use your api key'
+os.environ['SERPER_API_KEY'] = 'd669d5ffde2e5e6adc421710a3512c74c49e0d3d'
+os.environ["GOOGLE_API_KEY"] = 'AIzaSyAWNuQVMDHgqEMlIiqxr-Rk1EwFUjzIH3w'
+search_tool = SerperDevTool()
+
+llm = "gemini/gemini-2.5-flash"
+teacher = Agent(
+    role='Teacher',
+    goal='Explain concepts clearly step by step',
+    backstory='Experienced teacher who explains with examples',
+    llm=llm,
+    verbose=True
+)
+
+researcher = Agent(role = 'Researcher',
+                   goal='Find latest information',
+                   backstory = 'Expert in searching real-world data',
+                   tools=[search_tool],
+                   llm = llm,
+                   verbose = True)
+
+simplifier = Agent(role = 'Simplifier',
+                   goal = 'Make thing easy',
+                   backstory = 'Breaks complex ideas into simple language',
+                   llm = llm,
+                   verbose=True)
+
+student = Agent(role= 'Student',
+                goal = 'Take notes',
+                backstory = 'Writes simple notes like learner',
+                llm = llm,
+                verbose = True)
+
+examiner = Agent (role = 'Examiner',
+                 goal= 'Create questions',
+                 backstory = 'Tests understanding',
+                 llm = llm,
+                 verbose=True)
+
+topic = 'What is difference between agents vs agentic ai vs mcp vs generative ai'
+
+task1 = Task(
+    description = f'Explain{topic} in simple steps with examples',
+    expected_output= 'step-by-step explanation',
+    agent = teacher
+
+)
+task2 = Task(
+    description = f'search and find 3 important points about {topic}',
+    expected_output = '3 clear points',
+    agent = researcher
+)
+
+task3 = Task(
+    description = f'Simplify the explanation of {topic}',
+    expected_output='Very simple explanation',
+    agent = simplifier
+)
+
+task4 = Task(
+    description=f'Write short notes on {topic} like a student',
+    expected_output='Simple notes',
+    agent= student
+
+)
+
+task5 = Task(
+    description=f'Create 5 questions about {topic}',
+    expected_output='3 easy questions',
+    agent=examiner
+
+)
+
+
+crew = Crew(
+    agents=[teacher, researcher, simplifier, student, examiner],
+    tasks=[task1, task2, task3, task4, task5],
+    verbose=True
+)
+
+result = crew.kickoff()
+print(result)
---
+++ MultiAgent.py
@@ -0,0 +1,85 @@
+
+import os
+import gradio as gr
+from crewai import Agent, Task, Crew
+from crewai_tools import SerperDevTool
+
+os.environ['SERPER_API_KEY'] = 'd669d5ffde2e5e6adc421710a3512c74c49e0d3d'
+os.environ["GOOGLE_API_KEY"] = 'AIzaSyAWNuQVMDHgqEMlIiqxr-Rk1EwFUjzIH3w'
+search_tool = SerperDevTool()
+
+def run_multi_agent(topic):
+  teacher = Agent(role ='Teacher',
+                goal ='Explain concepts clearly step by step',
+                backstory = 'Experienced teacher who explain with example',
+                llm = 'gpt-4o-mini',
+                verbose = True)
+  researcher = Agent(role = 'Researcher',
+                   goal='Find latest information',
+                   backstory = 'Expert in searching real-world data',
+                   tools=[search_tool],
+                   llm = 'gpt-4o-mini',
+                   verbose = True)
+  simplifier = Agent(role = 'Simplifier',
+                   goal = 'Make thing easy',
+                   backstory = 'Breaks complex ideas into simple language',
+                   llm = 'gpt-4o-mini',
+                   verbose=True)
+  student = Agent(role= 'Student',
+                goal = 'Take notes',
+                backstory = 'Writes simple notes like learner',
+                llm = 'gpt-4o-mini',
+                verbose = True)
+  examiner = Agent (role = 'Examiner',
+                 goal= 'Create questions',
+                 backstory = 'Tests understanding',
+                 llm = 'gpt-4o-mini',
+                 verbose=True)
+
+  task1 = Task(
+    description = f'Explain{topic} in simple steps with examples',
+    expected_output= 'step-by-step explanation',
+    agent = teacher)
+
+  task2 = Task(
+    description = f'search and find 3 important points about {topic}',
+    expected_output = '3 clear points',
+    agent = researcher)
+
+  task3 = Task(
+    description = f'Simplify the explanation of {topic}',
+    expected_output='Very simple explanation',
+    agent = simplifier )
+
+  task4 = Task(
+    description=f'Write short notes on {topic} like a student',
+    expected_output='Simple notes',
+    agent= student)
+
+  task5 = Task(
+    description=f'Create 5 questions about {topic}',
+    expected_output='3 easy questions',
+    agent=examiner)
+
+  crew = Crew(
+      agents=[teacher, researcher, simplifier, student, examiner],
+      tasks=[task1, task2, task3, task4, task5],
+      verbose=False)
+
+  result = crew.kickoff()
+
+  return str(result)
+
+interface = gr.Interface(
+    fn = run_multi_agent,
+    inputs = gr.Textbox(
+        label = 'Enter Topic',
+        placeholder = 'what is Agentic AI vs single '),
+    outputs = gr.Textbox(label = 'Multi-Agent Output'),
+    title = 'Jagatjyoti Mohanty MultiAgent AI Training',
+    description = 'Enter any topic and see multiple AI agents colabotate to teach, simplify and test you'
+
+)
+
+if __name__ == '__main__':
+  interface.launch()
